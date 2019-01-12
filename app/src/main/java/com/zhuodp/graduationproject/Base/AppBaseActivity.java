913d731c619cdfd7f84756bcabea99691651a68d
zhuodp <740212253@qq.com>
2019-01-12 16:49:51 +0800

    add Base Activity and Frament

---
+++ app/src/main/java/com/zhuodp/graduationproject/Base/AppBaseActivity.java
@@ -0,0 +1,7 @@
+package com.zhuodp.graduationproject.Base;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class AppBaseActivity extends AppCompatActivity {
+
+}
---
+++ app/src/main/java/com/zhuodp/graduationproject/Base/AppBaseFragment.java
@@ -0,0 +1,7 @@
+package com.zhuodp.graduationproject.Base;
+
+import android.support.v4.app.Fragment;
+
+public class AppBaseFragment extends Fragment {
+
+}
