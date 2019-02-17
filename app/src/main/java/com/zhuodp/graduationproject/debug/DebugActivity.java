097b4cc526fef952cf6ec0eb40e8cd5e48adfc6e
zhuodp <740212253@qq.com>
2019-02-17 21:14:09 +0800

    add dialog

---
+++ app/src/main/java/com/zhuodp/graduationproject/debug/DebugActivity.java
@@ -0,0 +1,29 @@
+package com.zhuodp.graduationproject.debug;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+import com.zhuodp.graduationproject.Base.AppBaseActivity;
+import com.zhuodp.graduationproject.R;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+public class DebugActivity extends AppBaseActivity {
+
+    @BindView(R.id.btn_debug_activity)
+    Button test1;
+
+    @OnClick(R.id.btn_debug_activity)
+    public void onTestButtonClick(){
+        Intent intent = new Intent(DebugActivity.this,)
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+    }
+}
---
+++ commondialog/src/main/java/com/zhuodp/commondialog/dialogActivity.java
@@ -0,0 +1,12 @@
+package com.zhuodp.commondialog;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class dialogActivity extends Activity {
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.dialog_activity);
+    }
+}
