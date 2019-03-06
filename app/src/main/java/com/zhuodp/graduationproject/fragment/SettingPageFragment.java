b9bf57c5e2d61a4d5c7241617bc32a11d63bd9c2
zhuodp <740212253@qq.com>
2019-03-06 14:19:54 +0800

    Dialog Test Passed

--- app/src/main/java/com/zhuodp/graduationproject/fragment/SettingPageFragment.java
+++ app/src/main/java/com/zhuodp/graduationproject/fragment/SettingPageFragment.java
@@ -1,15 +1,18 @@
 package com.zhuodp.graduationproject.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import com.zhuodp.graduationproject.Base.AppBaseFragment;
 import com.zhuodp.graduationproject.R;
 import com.zhuodp.graduationproject.adapter.SettingListAdapter;
+import com.zhuodp.graduationproject.debug.DebugActivity;
 import com.zhuodp.graduationproject.entry.SettingItem;
 
 import java.util.ArrayList;
@@ -44,11 +47,22 @@ public class SettingPageFragment extends AppBaseFragment {
         mSettingItems.add(new SettingItem("意见反馈",R.drawable.ic_menu_camera));
         mSettingItems.add(new SettingItem("更多应用",R.drawable.ic_menu_camera));
         mSettingItems.add(new SettingItem("其他设置",R.drawable.ic_menu_camera));
+        mSettingItems.add(new SettingItem("测试入口",R.drawable.ic_menu_manage));
 
     }
 
     private void initSettings(){
         mSettingListAdapter =new SettingListAdapter(getActivity(),mSettingItems);
         mSettingListView.setAdapter(mSettingListAdapter);
+
+        mSettingListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (position==5){
+                    Intent intent = new Intent(getActivity(),DebugActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
     }
 }
