482657ac74e9c10d6e4f2e1f88cae68c0dfe98a8
zhuodp <740212253@qq.com>
2019-03-08 14:03:35 +0800

    move banner to home page  fragment

--- app/src/main/java/com/zhuodp/graduationproject/fragment/HomePageFragment.java
+++ app/src/main/java/com/zhuodp/graduationproject/fragment/HomePageFragment.java
@@ -9,14 +9,19 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
 import com.zhuodp.graduationproject.Base.AppBaseFragment;
 import com.zhuodp.graduationproject.adapter.TablayoutFragmentPagerAdapter;
 import com.zhuodp.graduationproject.R;
 import com.zhuodp.graduationproject.fragment.tab_fragment.CollectionFragment;
 import com.zhuodp.graduationproject.fragment.tab_fragment.HotPiontFragment;
 import com.zhuodp.graduationproject.fragment.tab_fragment.MovieFragment;
+import com.zhuodp.graduationproject.helper.GlideImageLoader;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import butterknife.BindView;
@@ -35,6 +40,19 @@ public class HomePageFragment extends AppBaseFragment {
     ViewPager mHomePageTabViewPager;
 
 
+    private String[]  mBannerImageUris ={
+            "https://b-ssl.duitang.com/uploads/item/201601/02/20160102175929_hATXs.jpeg"
+            ,"http://pic14.photophoto.cn/20100227/0036036381162387_b.jpg"
+            ,"http://5b0988e595225.cdn.sohucs.com/images/20180819/b742f087903d4bf7a7668f335106d145.jpeg"
+            ,"http://5b0988e595225.cdn.sohucs.com/q_70,c_zoom,w_640/images/20180728/14a1daaf28274d5a9f37d92da6e5b67a.jpeg"
+    };
+
+    private String[] mBannerImageTitle = {
+            "1","2","3","4"
+    };
+
+    @BindView(R.id.home_page_banner)
+    Banner mBanner;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState){
@@ -47,6 +65,51 @@ public class HomePageFragment extends AppBaseFragment {
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         initTabFragments();
+        initBanner();
+
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        //切回界面时开始轮播
+        mBanner.startAutoPlay();
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        //切出界面时停止轮播
+        mBanner.stopAutoPlay();
+    }
+
+    private void initBanner(){
+        //设置banner样式
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        //设置图片加载器
+        mBanner.setImageLoader(new GlideImageLoader());
+        //设置图片集合
+        mBanner.setImages(Arrays.asList(mBannerImageUris));
+        mBanner.setBannerAnimation(Transformer.DepthPage);
+        //设置标题集合（当banner样式有显示title时）
+        mBanner.setBannerTitles(Arrays.asList(mBannerImageTitle));
+        //自动轮播
+        mBanner.isAutoPlay(true);
+        //轮播时间
+        mBanner.setDelayTime(2000);
+        //设置指示器位置
+        mBanner.setIndicatorGravity(BannerConfig.CENTER);
+        //开始banner展示
+        mBanner.start();
+
+    }
+
+    private void initTabFragments(){
+        mTabsFragments = new ArrayList<Fragment>();
+        mTabsFragments.add(new CollectionFragment());
+        mTabsFragments.add(new HotPiontFragment());
+        mTabsFragments.add(new MovieFragment());
+
         mHomePageTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -68,14 +131,6 @@ public class HomePageFragment extends AppBaseFragment {
         mHomePageTabLayout.setupWithViewPager(mHomePageTabViewPager);
         //必须在setupWithViewPager之后添加标签，否则标签内容会被内部方法清除掉
         addTabs();
-
-    }
-
-    private void initTabFragments(){
-        mTabsFragments = new ArrayList<Fragment>();
-        mTabsFragments.add(new CollectionFragment());
-        mTabsFragments.add(new HotPiontFragment());
-        mTabsFragments.add(new MovieFragment());
     }
 
     private void addTabs(){
