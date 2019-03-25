41e8e5baeab3ea649899e8e0dfc671fd6e3b8d92
zhuodp <you@example.com>
2019-03-26 01:15:18 +0800

    添加了确认登陆方式的dialog，待使用

---
+++ dialogs/src/main/java/com/youdao/lib/dialogs/activity/AskForLoginActivity.java
@@ -0,0 +1,62 @@
+package com.youdao.lib.dialogs.activity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.view.WindowManager;
+
+import com.youdao.lib.dialogs.R;
+import com.youdao.lib.dialogs.base.BaseDialogActivity;
+import com.youdao.lib.dialogs.manager.CustomDialogManager;
+
+public class AskForLoginActivity extends BaseDialogActivity {
+
+    CustomDialogManager mCustomDialogManager = CustomDialogManager.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //重设入场动画
+        overridePendingTransition(R.anim.slide_in_bottom,R.anim.slide_out_bottom);
+        setContentView(R.layout.dialog_ask_for_login);
+
+    }
+
+    // 设置弹窗出现的位置，即DecorView在PhoneWindow里的位置
+    @Override
+    public void onAttachedToWindow() {
+        super.onAttachedToWindow();
+        View view = getWindow().getDecorView();
+        WindowManager.LayoutParams lp = (WindowManager.LayoutParams) view.getLayoutParams();
+        lp.width = WindowManager.LayoutParams.MATCH_PARENT;
+        lp.height= WindowManager.LayoutParams.MATCH_PARENT;
+        lp.gravity = Gravity.BOTTOM | Gravity.CENTER_HORIZONTAL;
+        //lp.gravity = Gravity.CENTER;
+        lp.x = 0;
+        lp.y = 0;
+        //lp.width = 100;
+        lp.height = 100;
+        getWindowManager().updateViewLayout(view, lp);
+    }
+
+    @Override
+    public void finish(){
+        super.finish();
+        overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
+        //回收hashmap中的监听器对象
+        mCustomDialogManager.recycleListener(mCustomDialogManager.TYPE_EXPORT_DIALOG);
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+
+    public void onCanceled(View view){
+        mCustomDialogManager.performExportDialogClick(CustomDialogManager.TAG_EXPORT_DIALOG_CANCEL);
+        finish();
+    }
+
+
+
+}
